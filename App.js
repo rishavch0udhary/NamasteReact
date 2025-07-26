2ed9f2d943dafaed0db35b142871f161e14cf9dc
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-07-26 15:19:09 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+  const parent = React.createElement(
+    "div",
+    {id:"parent"},
+      React.createElement(
+        "div",
+        {id:"child"},
+       [ React.createElement("h1",{},"I'm H1 tag"),
+        React.createElement("h1",{},"I'm H2 tag")]
+      )
+  );
+
+    console.log(parent);
+    
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(parent);
